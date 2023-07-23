ae3f959d0c72f6ab8165d3a6a202061f85578516
Rob Smith <legendary.belief@gmail.com>
2023-07-22 21:02:39 -0700

    Added CSS and Fixed Formatting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-        <ExpenseItem></ExpenseItem>
-       
+        <p>Hello, World!</p>
+        <ExpenseItem />
       </header>
     </div>
   );
